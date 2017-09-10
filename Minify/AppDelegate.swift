010f239e8f0a07544578501fd56673cdf150c078
Ali Yasin Eser <aliyasineser@gmail.com>
2017-09-10 03:13:39 +0300

    Initial Commit

---
+++ Minify/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Minify
+//
+//  Created by aliyasineser on 10/09/2017.
+//  Copyright © 2017 aliyasineser. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
